d976a583e0d47bc6ee3f2bd7eef7296ce9fdea38
Gao Bin <bin_gao@live.cn>
2011-06-03 19:37:15 +0800

    add the generate type1 data format

--- js/gei-ex.js
+++ js/gei-ex.js
@@ -1,5 +1,16 @@
 var gei = gei || {};
 
+//data type 
+
+gei.type = {
+	type1:{
+		category1:"Macroeconomics",
+		name1:"Difference (2009 $ billions)",
+		category2:"Emissions",
+		name2:"Total"
+	}
+}
+
 //the raw format
 gei.generateRawValue = function(text,frtColumnName){
 	
@@ -176,6 +187,150 @@ gei.generateMotionValue = function(text,frtColumnName){
 						 den_array.push(newLine);
 					 }
 				 }
+			 }
+			 
+			 //the category
+			 if(notNullValueNum(row) == 1 && notNullValueNum(nextRow) == 0){
+				 currentCategory = row[1];
+			 } 
+			 
+			 //preRow is empty or data line ,current row is one data,next row is data line,it is a sub-category
+			 if(notNullValueNum(preRow) != 1 && notNullValueNum(row) == 1 && notNullValueNum(nextRow) > 1){
+				 currentSubCategory = row[1];
+			 }
+			 
+			 //current row is one data,next row is empty ,the next 2 row is data line,it's sub-category is the category
+			 if(notNullValueNum(row) == 1 && notNullValueNum(nextRow) == 0 && notNullValueNum(nextRow2) > 1){
+				 currentSubCategory = currentCategory;
+			 }
+			 
+			 //current row is one data,the next 2 rows is empty,it's sub-category is the category 
+			 if(notNullValueNum(row) == 1 && notNullValueNum(nextRow) == 0 && notNullValueNum(nextRow3) == 0){
+				 currentSubCategory = currentCategory;
+			 }
+			 
+			 //current row is one data,the next 2 rows are one data,it's sub-category is the category,sheet 13
+			 if(notNullValueNum(row) == 1 && notNullValueNum(nextRow) == 1 && notNullValueNum(nextRow2) == 1){
+				 currentSubCategory = row[1];
+			 }
+			 
+		 }
+		 
+		 var den_str = "";
+		 for(var a = 0; a < den_array.length; a++){
+			 den_str = den_str + den_array[a];
+		 }
+   
+		 return den_str;
+	 }
+};
+
+
+//the Type format
+gei.generateTypeValue = function(text,frtColumnName,dataType){
+	
+	 if (text == ""){
+		 return null;
+	 }else{ 
+		 var den_array = new Array();
+		 var text_array = new Array();   
+		 text_array = bulidTextArrayFromCSV(text);   
+		 var category1, name1, category2, name2;
+		 
+		 if(dataType = "type1"){
+			 category1 = gei.type.type1.category1;
+			 name1 = gei.type.type1.name1;
+			 category2 = gei.type.type1.category2;
+			 name2 = gei.type.type1.name2;
+		 }
+
+		 //change the format to Denormalized
+		 var firsLine = [];
+		 var firstRow = [];
+		 
+		 //here build the table title column
+		 firsLine.push("Scenario");
+		 firsLine.push("Category");
+		 firsLine.push("SubCategory");
+		 firsLine.push("Name");
+		 firsLine.push("Year");
+		 firsLine.push("Value");
+		 firsLine.push("Total");
+		 firsLine.push("\n");
+		 den_array.push(firsLine);
+		 
+		 //here build the year value
+		 var yearArr = [];
+		 if(text_array.length > 1){
+			 //sometimes in the csv file the first row have nothing or only have S1 or S5,this is no use
+			 //so the first row should be the file title
+			 if(notNullValueNum(text_array[0]) == 0 || text_array[0][0] != ""){
+				 firstRow = text_array[1];
+			 }else{
+				 firstRow = text_array[0];
+			 }
+			 for(var t = 2; t < firstRow.length; t++){
+				 yearArr.push(firstRow[t]);
+			 } 
+		 }
+		 
+		 
+		 var currentCategory = "";
+		 var currentSubCategory = "";
+		 
+		 //here build each line data
+		 //you will note here start with 2,
+		 //because the first line maybe is the null line and the second line maybe the column title
+		 //or the first line is the column title and the second line is null line
+		 for(var j = 2; j < text_array.length; j++){
+			 var preRow = text_array[j-1];
+			 var row = text_array[j];
+			 var nextRow = text_array[j+1];
+			 var nextRow2 = text_array[j+2];
+			 var nextRow3 = text_array[j+3];
+			 
+			 //the null line
+			 if(notNullValueNum(row) == 0){
+				 continue;
+			 }
+			 
+			 //the data line 
+			 if(notNullValueNum(row) > 1){
+				 var name = row[1];
+
+				 if(currentCategory == category1 && name == name1){
+					 for(var k = 2; k < row.length; k++){
+						 if(row[k] != ""){
+							 var newLine = new Array();
+							 newLine.push(frtColumnName);
+							 newLine.push(currentCategory);
+							 newLine.push(currentSubCategory);
+							 newLine.push(name);
+							 newLine.push(yearArr[k-2]);
+							 newLine.push(row[k]);
+							 newLine.push("");
+							 newLine.push("\n");
+							 den_array.push(newLine);
+						 }
+					 }
+				 }
+				 
+				 if(currentCategory == category2 && name == name2){
+					 var total_array = new Array();
+					 for(var k = 2; k < row.length; k++){
+						 if(row[k] != ""){
+							 total_array.push(row[k]);
+						 }
+					 }
+					 
+					 for(var h = 1; h < den_array.length; h++){
+						 var denRow = den_array[h];
+						 if(h < total_array.length+1){
+							 den_array[h][6] = total_array[h-1];
+						 } 
+					 }
+				 }
+				 
 			 }
 			 
 			 //the category
